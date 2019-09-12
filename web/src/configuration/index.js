3f8c15fbc04e454e6f82d71948a1aa55d35ea9a1
ethanae <eemandien@mweb.co.za>
2019-09-12 22:17:43 +0200

    - add config file

---
+++ web/src/configuration/index.js
@@ -0,0 +1,6 @@
+export const rinkebyProviderUrl = 'https://rinkeby.infura.io/jkWNMTe51IxgXBcG9G4i';
+
+export const investmentFactoryContractAddress =  '0x64134384DCcAF62CDeCF1CD43790E44Efe9Fd635';
+export const investmentRankingContractAddress = '0x1B4209E607d03B831895671a29b6d2db5e709d2e';
+
+export const etherScanApiKey = `3QXCJX1T884HESW59QWT99E868DC5MJYCE`;
